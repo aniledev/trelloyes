802d2c317a9a211e671fff520d2fc7a3abf95585
aniledev <elinamcgill@gmail.com>
2020-12-02 22:47:16 -0500

    feat: add App component to store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import STORE from "./store";
 import List from "./composition/List";
 import Card from "./composition/Card";
 
-function App(props) {
-  return (
-    <main className="App">
-      <header>
-        <h1>{/* content goes here */}</h1>
-      </header>
-      <div className="App-list"></div>
-    </main>
-  );
+// WHAT EXACTLY IS A CLASS AND HOW DOES IT FUNCTION?
+class App extends Component {
+  // default props before render statement. HOW IS STATIC FUNCTIONING IN THIS STATEMENT?
+  static defaultProps = {
+    STORE: {
+      lists: [],
+      allCards: {},
+    },
+  };
+  // the render function
+  render() {
+    const { STORE } = this.props;
+    // JSX elements that are returned from the function
+    return (
+      <main className="App">
+        <header>
+          <h1>Trelloyes</h1>
+        </header>
+        <div className="App-list">
+          {STORE.lists.map((list) => (
+            <List
+              key={list.id}
+              header={list.header}
+              // MAKE SURE THAT YOU UNDERSTAND EACH PIECE OF THIS MAP FUNCTION
+              cards={list.cardIds.map((id) => STORE.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
