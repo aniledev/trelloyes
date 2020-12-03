8a78720e266740f5d301e60a94ff7960df8a9643
aniledev <elinamcgill@gmail.com>
2020-12-03 16:12:07 -0500

    feat: Pass STORE as a prop within index.js file
    This change allowed both the List and Card component to render properly

--- src/composition/List.js
+++ src/composition/List.js
@@ -1,16 +1,14 @@
 import React from "react";
-import Card from "../composition/Card";
+import Card from "./Card";
 import "./List.css";
 
-function List(props) {
+export default function List(props) {
   return (
     <section className="List">
       <header className="List-header">
-        {/* WHAT EXACTLY IS PROPS.HEADER REFFERING TO.  */}
         <h2>{props.header}</h2>
       </header>
       <div className="List-cards">
-        {/* WHAT EXACTLY IS PROPS.CARDS REFFERING TO. I UNDERSTAND THE MAP FUNCTION BUT DON'T KNOW WHAT THIS REFERENCE IS */}
         {props.cards.map((card) => (
           <Card key={card.id} title={card.title} content={card.content} />
         ))}
@@ -18,5 +16,3 @@ function List(props) {
     </section>
   );
 }
-
-export default List;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import STORE from "./STORE";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App STORE={STORE} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
