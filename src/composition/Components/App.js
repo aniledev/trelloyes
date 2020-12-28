badd000439f913059cdefbfdc0dc659b1b0cd9bc
aniledev <elinamcgill@gmail.com>
2020-12-27 21:04:17 -0500

    refractor: add id prop to Card component  / replace hardcoded state data with STORE module

--- src/composition/Components/App.js
+++ src/composition/Components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../Styles/App.css";
 import List from "./List";
+import STORE from "../../STORE";
 
-// this component will contain the state for the application because it is the lowest common ancestor that can read and update state
 function omit(obj, keyToOmit) {
   return Object.entries(obj).reduce(
     (newObj, [key, value]) =>
@@ -11,84 +11,28 @@ function omit(obj, keyToOmit) {
   );
 }
 
+// this component will contain the state for the application because it is the lowest common ancestor that can read and update state
 class App extends React.Component {
   //initialize state for App component using test data
   state = {
-    lists: [
-      {
-        id: "1",
-        header: "First list",
-        cardIds: ["a", "b", "e", "f", "g", "j", "l", "m"],
-      },
-      {
-        id: "2",
-        header: "Second list",
-        cardIds: ["b", "c", "d", "f", "h", "i", "k"],
-      },
-      {
-        id: "3",
-        header: "Third list",
-        cardIds: [
-          "a",
-          "b",
-          "c",
-          "d",
-          "e",
-          "f",
-          "g",
-          "h",
-          "i",
-          "j",
-          "k",
-          "l",
-          "m",
-        ],
-      },
-      {
-        id: "4",
-        header: "Fourth list",
-        cardIds: ["l", "m"],
-      },
-    ],
-    allCards: {
-      a: { id: "a", title: "First card", content: "lorem ipsum" },
-      b: { id: "b", title: "Second card", content: "lorem ipsum" },
-      c: { id: "c", title: "Third card", content: "lorem ipsum" },
-      d: { id: "d", title: "Fourth card", content: "lorem ipsum" },
-      e: { id: "e", title: "Fifth card", content: "lorem ipsum" },
-      f: { id: "f", title: "Sixth card", content: "lorem ipsum" },
-      g: { id: "g", title: "Seventh card", content: "lorem ipsum" },
-      h: { id: "h", title: "Eighth card", content: "lorem ipsum" },
-      i: { id: "i", title: "Ninth card", content: "lorem ipsum" },
-      j: { id: "j", title: "Tenth card", content: "lorem ipsum" },
-      k: { id: "k", title: "Eleventh card", content: "lorem ipsum" },
-      l: { id: "l", title: "Twelfth card", content: "lorem ipsum" },
-      m: { id: "m", title: "Thirteenth card", content: "lorem ipsum" },
-    },
-  };
-
-  static defaultProps = {
-    STORE: {
-      lists: [],
-      allCards: {},
-    },
+    store: STORE,
   };
 
   // DON'T FORGET TO USE ARROW FUNCTIONS SO WE AVOID UNDEFINED ERRORS
-
+  // this function will handle the delete callback prop
   handleDeleteCard = (cardId) => {
-    // this function will handle the delete callback prop
-    // use object destructuing to access the current state
-    const { lists, allCards } = this.state;
+    const { lists, allCards } = this.state.store;
 
     // create a new array of the list using a map array method
     const newLists = lists.map((list) => ({
       ...list,
       cardIds: list.cardIds.filter((id) => id !== cardId),
     }));
+
     // create a new array of the cards minus the deleted card using the omit function
     const newCards = omit(allCards, cardId);
 
+    console.log("delete clicked");
     this.setState({
       store: {
         lists: newLists,
@@ -98,28 +42,48 @@ class App extends React.Component {
   };
 
   // this function will handle the add callback prop
-  handleAddCard = () => {
-    console.log("handle add clicked");
-  };
+  // handleAddCard = (listId) => {
+  //   const newCard = newRandomCard();
+
+  //   const newLists = this.state.store.lists.map((list) => {
+  //     if (list.id === listId) {
+  //       return {
+  //         ...list,
+  //         cardIds: [...list.cardIds, newCard.id],
+  //       };
+  //     }
+  //     return list;
+  //   });
+
+  //   this.setState({
+  //     store: {
+  //       lists: newLists,
+  //       allCards: {
+  //         ...this.state.store.allCards,
+  //         [newCard.id]: newCard,
+  //       },
+  //     },
+  //   });
+  // };
 
   render() {
-    // use obejct destructuring to access this.state keys
-    const { lists, allCards } = this.state;
+    // use object destructuing to access the current state
+    const { store } = this.state;
 
     return (
       <main className="App">
         <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-
         <div className="App-list">
-          {lists.map((list) => (
+          {store.lists.map((list) => (
             <List
               key={list.id}
               id={list.id}
               header={list.header}
-              cards={list.cardIds.map((id) => allCards[id])}
+              cards={list.cardIds.map((id) => store.allCards[id])}
               onClickDelete={this.handleDeleteCard}
+              onClickAdd={this.handleAddCard}
             />
           ))}
         </div>
--- src/composition/Components/List.js
+++ src/composition/Components/List.js
@@ -2,21 +2,22 @@ import React from "react";
 import Card from "./Card";
 import "../Styles/List.css";
 
+// props are passed in from the App component
 export default function List(props) {
   return (
     <section className="List">
       <header className="List-header">
-        {/* props are passed in from the App component */}
-        {/* pass in onClickDelete callback prop so it gets passed back up to the App component */}
         <h2>{props.header}</h2>
       </header>
       <div className="List-cards">
         {props.cards.map((card) => (
           <Card
             key={card.id}
+            id={card.id}
             title={card.title}
             content={card.content}
             onClickDelete={props.onClickDelete}
+            //pass in onClickDelete callback prop so it gets passed back up to the App component
           />
         ))}
       </div>
