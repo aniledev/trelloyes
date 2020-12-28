8cab7dd8a5c287bd01b99db8a857e53efd49b12e
aniledev <elinamcgill@gmail.com>
2020-12-27 20:11:57 -0500

    refractor: move return statement outside of render method in List function component

--- src/composition/Components/List.js
+++ src/composition/Components/List.js
@@ -2,28 +2,28 @@ import React from "react";
 import Card from "./Card";
 import "../Styles/List.css";
 
-export default function List (props){
- 
-
-  render() {
-    return (
-      <section className="List">
-        <header className="List-header">
-          {/* props are passed in from the App component */}
-          {/* pass in onClickDelete callback prop so it gets passed back up to the App component */}
-          <h2>{props.header}</h2>
-        </header>
-        <div className="List-cards">
-          {props.cards.map((card) => (
-            <Card key={card.id} title={card.title} content={card.content}
-              onClickDelete={props.onClickDelete}/>
-          ))}
-        </div>
-      </section>
-    );
-  };
+export default function List(props) {
+  return (
+    <section className="List">
+      <header className="List-header">
+        {/* props are passed in from the App component */}
+        {/* pass in onClickDelete callback prop so it gets passed back up to the App component */}
+        <h2>{props.header}</h2>
+      </header>
+      <div className="List-cards">
+        {props.cards.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            content={card.content}
+            onClickDelete={props.onClickDelete}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 List.defaultProps = {
   onClickAdd: () => {},
-}
+};
